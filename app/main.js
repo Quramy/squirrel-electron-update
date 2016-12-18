569cd976dc41bf0c04e4b2f73db63c969d7316cb
y-kurami <yosuke.kurami@gmail.com>
2016-12-19 04:49:10 +0900

    update

--- app/main.js
+++ app/main.js
@@ -1,8 +1,23 @@
-const {app, BrowserWindow} = require('electron');
+const {app, autoUpdater, BrowserWindow} = require('electron');
 
 let win;
 
+const feedURL = "https://squirrel-electron-update.herokuapp.com/download";
 
+if (process.platform === "win32") {
+  autoUpdater.setFeedURL(feedURL);
+  autoUpdater.checkForUpdates();
+  autoUpdater.on('update-download', () => {
+    console.log('found update!');
+  });
+  autoUpdater.on('update-not-available', () => {
+    console.log('not found...');
+  });
+
+  autoUpdater.on('error', () => {
+    console.log('error');
+  });
+}
 
 app.on('ready', () => {
   win = new BrowserWindow();
